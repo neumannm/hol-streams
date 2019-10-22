09b5f6936182b1e8d8def00493940963212af4db
Mandy Neumann <mandy.neumann@th-koeln.de>
2019-10-22 16:02:59 +0200

    Add my Unit1 solutions

--- src/main/java/com/speedment/example/solution/Unit1MyCreate.java
+++ src/main/java/com/speedment/example/solution/Unit1MyCreate.java
@@ -3,6 +3,8 @@ package com.speedment.example.solution;
 import com.speedment.example.unit.Unit1Create;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Random;
 import java.util.stream.IntStream;
@@ -10,43 +12,43 @@ import java.util.stream.Stream;
 
 public final class Unit1MyCreate implements Unit1Create {
 
-    @Override
-    public Stream<String> newStreamOfAToC() {
-        return Stream.empty();
-    }
+	@Override
+	public Stream<String> newStreamOfAToC() {
+		return Stream.of("A", "B", "C");
+	}
 
-    @Override
-    public IntStream newIntStreamOfOneToSeven() {
-        return IntStream.empty();
-    }
+	@Override
+	public IntStream newIntStreamOfOneToSeven() {
+		return IntStream.rangeClosed(1, 7);
+	}
 
-    @Override
-    public Stream<String> from(String[] array) {
-        return Stream.empty();
-    }
+	@Override
+	public Stream<String> from(String[] array) {
+		return Stream.of(array);
+	}
 
-    @Override
-    public Stream<String> from(Collection<String> collection) {
-        return Stream.empty();
-    }
+	@Override
+	public Stream<String> from(Collection<String> collection) {
+		return collection.stream();
+	}
 
-    @Override
-    public IntStream from(String s) {
-        return IntStream.empty();
-    }
+	@Override
+	public IntStream from(String s) {
+		return s.chars();
+	}
 
-    @Override
-    public IntStream infiniteAlternating() {
-        return IntStream.empty();
-    }
+	@Override
+	public IntStream infiniteAlternating() {
+		return IntStream.iterate(1, i -> i * -1);
+	}
 
-    @Override
-    public IntStream infiniteRandomInts(Random rnd) {
-        return IntStream.empty();
-    }
+	@Override
+	public IntStream infiniteRandomInts(Random rnd) {
+		return rnd.ints();
+	}
 
-    @Override
-    public Stream<String> linesFromPoemTxtFile() throws IOException {
-        return Stream.empty();
-    }
+	@Override
+	public Stream<String> linesFromPoemTxtFile() throws IOException {
+		return Files.lines(Paths.get("poem.txt"));
+	}
 }
